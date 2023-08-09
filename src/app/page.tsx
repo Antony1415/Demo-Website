45527419091aaf223fa970da947e7fb20b3410e6
Antony <antonyzz123@gmail.com>
2023-08-09 15:06:36 +0700

    push to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,75 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect } from 'react'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <div className={styles.container}>
+      <center>
+        <p className={`${styles.container_title} mb-[50px]`}>Link Download Fast and Furious 10, Nonton Film Fast X Terbaru Yang Dibintangi Para Aktor Film Populer</p>
+      </center>
+      
+      <center>
+        <img src='fast_x.webp' alt='' className='mb-[40px]'/>
+      </center>
+
+      <div className={`${styles.container_content} text-justify grid gap-[20px]`}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Fast X (atau Fast & Furious 10) adalah sebuah film aksi asal Amerika tahun 2023 yang disutradarai Louis Leterrier dari skenario Dan Mazeau dan Justin Lin. Film ini dibuat berdasarkan cerita karangan Mazeau, Lin, dan Zach Dean. Film ini adalah sekuel dari F9 (2021), dan seri kesepuluh dari waralaba Fast & Furious.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+        <p>
+          Film ini dibintangi oleh Vin Diesel, Michelle Rodriguez, Tyrese Gibson, Chris Ludacris Bridges, Jason Momoa, Nathalie Emmanuel, Jordana Brewster, John Cena, Jason Statham, Sung Kang, Alan Ritchson, Daniela Melchior, Scott Eastwood, Helen Mirren, Charlize Theron, Brie Larson, and Rita Moreno.
+        </p>
+
+        <p>
+          Dengan perkiraan anggaran produksi $340 juta, ini menjadikannya film termahal ketujuh yang pernah dibuat. Syuting berlangsung di London, Rome, Turin, Lisbon, dan Los Angeles. Berikut sinopsis dari film Fast X.
+        </p>
+      </div>
+
+      <div className={`${window.innerWidth <= 1200 ? '' : 'flex my-[20px] gap-5'}`}>
+        <div className={`${window.innerWidth > 1200 && 'min-w-[250px] w-[calc(100vw-400px)] h-[250px] bg-slate-200 flex justify-center items-center'}
+          ${(window.innerWidth <= 1200 && window.innerWidth >= 700) &&
+          'mx-[50px] my-[20px] min-w-[150px] rounded-[5px] h-[250px] bg-slate-200 flex justify-center items-center'}
+          ${window.innerWidth < 700 && 'mx-[25px] my-[20px] min-w-[150px] rounded-[5px] h-[150px] bg-slate-200 flex justify-center items-center'}`}>
+          <p>Advertisement</p>
+        </div>
+
+        <div className={`${styles.container_content} flex flex-col gap-[20px]`}>
+          <p>
+            Sesuai dengan judul dari cerita tersebut film ini mengisahkan aksi dan juga kasus-kasus yang perlu diselesaikan. Penonton akan melihat aksi balapan antar pemain dengan visual dan juga sinematograpi sangatlah menarik. 
+          </p>
+          <p>
+            Wajar saja jika orang-orang terus menantikan seri terbaru dari film tersebut karena memang tidak pernah membosankan. Kabar baik bagi seluruh penggemar film Fast & Furious dimana pun kalian berada bahwa season 10 dari film tersebut akan rilis resmi pada tahun 2023. 
+          </p>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <p className={`${styles.container_title} mt-[50px] mb-[50px]`}>Fakta Terkait Dengan Film Fast & Furious</p>
+
+      <div className={`${styles.container_content} text-justify grid gap-[20px]`}>
+        <p>
+          Bisa dibilang film ini cukup booming di setiap kali penayangannya karena memang tiap serinya selalu ditunggu oleh para penggemar. Kisah cerita yang dibawakan juga sangat apik dan mengusung konsep action dan alur yang luar biasa. Ada beberapa fakta menarik yang perlu penonton ketahui terkait dengan film fast X ini. Buat kamu yang kecewa karena Dwayne Johnson tidak ada dalam daftar pemain di Fast 9.
+        </p>
+
+        <p>
+          Maka sepertinya di season yang akan tayang tahun 2023 ini aktor tersebut akan kembali ikut bermain dengan peran yang cukup penting. Hal tersebut diungkapkan sendiri oleh salah satu aktor dari Fast X, yakni Vin Diesel melalui laman instagram pribadinya. 
+        </p>
+
+        <p>
+          Kalian bisa lihat langsung postingan yang diunggah oleh Vin Diesel dan besar kemungkinan aktor yang sempat absen pada season sebelumnya itu akan hadir kembali. 
+        </p>
+
+        <p>
+          Kalian bisa lihat langsung postingan yang diunggah oleh Vin Diesel dan besar kemungkinan aktor yang sempat absen pada season sebelumnya itu akan hadir kembali. Selain itu juga, proses produksi dari film ini sempat terjadi masalah antara sutradara Justin Lin dengan Vin Diesel. Namun, dikabarkan juga kalau Justin Lin akan tetap menyutradain film Fast & Furious 10 ini.
+        </p>
+
+        <p>
+          Nah, para penggemar juga perlu tahu kalau seri dari Fast & Furious akan berakhir pada season ke 11. Jadi, masih ada dua seri lagi yang bisa kalian tonton sebelum akhirnya tamat.
+        </p>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+import styles from '../../app/page.module.css'
+
+const Aside = () => {
+  return (
+    <div className='flex flex-col justify-around'>
+        {window.innerWidth <= 900 && (
+            <div className={`${styles.aside} bg-gray-300 opacity-50 mb-[100px]`}>
+                <p className={styles.container_content}>Advertisement</p>
+            </div>
+        )}
+
+    <div className={`${styles.aside} bg-gray-300 opacity-50`}>
+        <p className={styles.container_content}>Advertisement</p>
+    </div>
+    </div>
+  )
+}
+
+export default Aside
